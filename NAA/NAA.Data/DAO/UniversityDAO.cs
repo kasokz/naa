314312fd77dd5cd9bb0aad843542a77b18472282
Knarzbob <d.kasper@gmx.net>
2016-06-02 10:40:42 +0200

    Added UniversityDAO

---
+++ NAA/NAA.Data/DAO/UniversityDAO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAA.Data.DAO
+{
+    class UniversityDAO
+    {
+        private NAAEntities _context;
+
+        public UniversityDAO() {
+            _context = new NAAEntities();
+        }
+
+        public IList<University> GetUniversity()
+        {
+            IQueryable<University> _universities;
+            _universities = from university
+                            in _context.University
+                            select university;
+            return _universities.ToList<University>();
+        }
+
+        public University GetUniversityById();
+
+        public void AddUniversity(University University);
+
+        public void EditUniversity(University University);
+
+        public void DeleteUniversityById(int id);
+    }
+}
--- NAA/NAA.Data/IDAO/IUniversityDAO.cs
+++ NAA/NAA.Data/IDAO/IUniversityDAO.cs
@@ -8,5 +8,14 @@ namespace NAA.Data.IDAO
 {
     interface IUniversityDAO
     {
+        public IList<University> GetUniversity();
+
+        public University GetUniversityById();
+
+        public void AddUniversity(University University);
+
+        public void EditUniversity(University University);
+
+        public void DeleteUniversityById(int id);
     }
 }
